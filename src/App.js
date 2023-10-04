0f417b61aa2b908e1903c12f1eee6cae2d05b630
Abduraxmon05 <abduraxmonbozorov05@gmail.com>
2023-10-04 14:23:06 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { Odam } from './components/odam';
+
+function App() {
+  return (
+    <div className='container border border-dark p-3'>
+      <Odam />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/odam.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Odam(){
+    return (
+        <div className="card">
+            <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fs.hdnux.com%2Fphotos%2F51%2F23%2F24%2F10827008%2F4%2F1200x0.jpg&tbnid=wBxCPpWjCGoe6M&vet=12ahUKEwjnu470gdyBAxUVzSoKHUeSAmMQMygSegQIARBR..i&imgrefurl=https%3A%2F%2Fwww.chron.com%2Fculture%2Fmain%2Fslideshow%2FSAT-scores-of-famous-people-81475.php&docid=8UDkHXS11uG2CM&w=1200&h=798&q=people&ved=2ahUKEwjnu470gdyBAxUVzSoKHUeSAmMQMygSegQIARBR" alt="person1" />
+            <div className="card-body"> 
+                <h1 className="card-title">Odam1</h1>
+                <p className="card-text">vfduhv ersiuh ers weriughewruiwedslkkjvnes u grewr w gwer gwergwerre w</p>
+            </div>
+
+            {console.log("Salom")}
+        </div>
+
+        
+    )
+}
+
+
+export {Odam};
